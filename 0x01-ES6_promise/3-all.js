1515afe6766ace37a88eac745184bd45602e18b5
Ahmad <ahmad.new.m.v@gmail.com>
2024-10-11 15:09:35 +0000

    4th task's solution

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,10 @@
+import { uploadPhoto, createUserAccount } from './utils';
+
+export default function handleProfileSignup() {
+  const photo = uploadPhoto();
+  const account = createUserAccount();
+
+  Promise.all([photo, account]).then((array) => {
+    console.log(`${array[0].body} ${array[1].firstName} ${array[1].lastName}`);
+  });
+}
