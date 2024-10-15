ee73bc541476dc9f46a99d6d8bc2cacc82c77130
Ahmad <ahmad.new.m.v@gmail.com>
2024-10-15 10:14:59 +0000

    Map validation

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,5 +1,8 @@
 export default function updateUniqueItems(map) {
   const unique = [];
+  if (map.toString() !== '[object Map]') {
+    return;
+  }
   map.forEach((value, key) => {
     if (value === 1) {
       unique.push(key);
