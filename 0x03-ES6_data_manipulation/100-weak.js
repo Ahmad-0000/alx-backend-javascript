4d8aea2779ad7c674c27f59be92286b5573f3ab0
Ahmad <ahmad.new.m.v@gmail.com>
2024-10-15 11:35:46 +0000

    Removed the bug

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,12 +1,13 @@
 const weakMap = new WeakMap();
 weakMap.set({ protocol: 'http', name: 'getUsers' });
 function queryAPI(endpoint) {
-  const times = weakMap.get(endpoint);
+  let times = weakMap.get(endpoint);
   if (times === undefined) {
     weakMap.set(endpoint, 1);
     return;
   }
-  weakMap.set(endpoint, times + 1);
+  times += 1;
+  weakMap.set(endpoint, times);
   if (times >= 5) {
     throw new Error('Endpoint load is high');
   }
