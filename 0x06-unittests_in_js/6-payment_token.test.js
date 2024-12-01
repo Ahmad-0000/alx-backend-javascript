cd08c702d47cc4cea032a38b6e4c90ee90d7816c
Ahmad <ahmad.new.m.v@gmail.com>
2024-12-01 08:23:49 +0000

    Removed a bug

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -6,7 +6,10 @@ const expect = chai.expect;
 describe('getPaymentTokenFromAPI', function () {
   it('tests getPaymentTokenFromAPI function', function (done) {
     getPaymentTokenFromAPI(true).then((data) => {
-      expect(data).to.deep.equal({data: 'Successful response from the API'});
+      expect(data).to.have.property('data');
+      expect(data.data).to.equal('Successful response from the API');
+    }).catch((error) => {
+      done(error);
     });
     done();
   });
