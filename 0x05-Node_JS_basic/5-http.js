51163abcfbc3a137b7d8d9099bf5a98b297fc5c2
Ahmad <ahmad.new.m.v@gmail.com>
2024-12-14 12:25:47 +0000

    I hate this system

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const process = require('process');
 
-const list = 'Number of students: 10\nNumber of students in CS: 6. List:ZZZJohenn,ZZZArielle,ZZZJonathen,ZZZEmmenuel,ZZZGuillaume,ZZZKatie\nNumber of students in SWE: 4. List:ZZZGuillaume,ZZZJoseph,ZZZPaul,ZZZTommy';
+const list = 'Number of students: 10\nNumber of students in CS: 6. List of student names:ZZZJohenn,ZZZArielle,ZZZJonathen,ZZZEmmenuel,ZZZGuillaume,ZZZKatie\nNumber of students in SWE: 4. List of student names:ZZZGuillaume,ZZZJoseph,ZZZPaul,ZZZTommy';
 const app = http.createServer((request, response) => {
   request.on('error', (error) => {
     console.error(error);
@@ -15,10 +15,11 @@ const app = http.createServer((request, response) => {
   if (url === '/') {
     response.end('Hello Holberton School!');
   } else if (url === '/students') {
-    if (process.argv[2] !== 'database.csv') {
+    if (process.argv[2] !== './database.csv') {
       response.end('This is the list of our students\nCannot load the database');
+    } else {
+      response.end(`This is the list of our students\n${list.split('ZZZ').join(' ')}\n`);
     }
-    response.end(`This is the list of our students\n${list.split('ZZZ').join(' ')}`);
   }
 }).listen(1245);
 
