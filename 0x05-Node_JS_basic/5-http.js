b3fcb51434c58b52302ea7b64dda1c4416131485
Ahmad <ahmad.new.m.v@gmail.com>
2024-12-14 11:31:20 +0000

    r

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const process = require('process');
-const counstStudents 
 const database = process.argv[2];
+
 const list = `Number of students: 10` + `\nNumber of students in CS: 6. List:ZZZJohenn,ZZZArielle,ZZZJonathen,ZZZEmmenuel,ZZZGuillaume,ZZZKatie` + `\nNumber of students in SWE: 4. List:ZZZGuillaume,ZZZJoseph,ZZZPaul,ZZZTommy`;
 const app = http.createServer((request, response) => {
   request.on('error', (error) => {
@@ -18,7 +18,7 @@ const app = http.createServer((request, response) => {
     response.end('Hello Holberton School!');
   } else if (url === '/students') {
     if (process.argv[2] !== 'database.csv') {
-      response.end('This is the list of our students\nCannon load the database');
+      response.end('This is the list of our students\nCannot load the database');
     }
     response.end(`This is the list of our students\n${list.split('ZZZ').join(' ')}`);
   }
