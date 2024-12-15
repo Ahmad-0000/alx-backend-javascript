eb923e029b5b206b164146fe580db8a5f1b3c61a
Ahmad <ahmad.new.m.v@gmial.com>
2024-12-15 06:50:41 +0200

    Compact tests

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -5,23 +5,13 @@ const chai = require('chai');
 const expect = chai.expect;
 
 describe('Test API', function () {
-  it('tests status code', function () {
+  it('tests root route', function () {
     requests.get('http://localhost:7865/', (error, response, body) => {
       if (error) {
         throw new Error();
       } else if (response) {
         expect(response.statusCode).to.equal(200);
-      }
-    });
-  });
-  it('tests body content', function () {
-    requests.get('http://localhost:7865/', (error, response, body) => {
-      if (error) {
-        throw new Error();
-      } else if (body) {
-        expect(body).to.not.equal('Welcome to the payment system');
-      } else {
-        throw new Error();
+        expect(body).to.contain('Welcome to the payment system');
       }
     });
   });
