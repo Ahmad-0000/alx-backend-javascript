aa4db29cf5f8541552c55dd3251d0d2bcb31ac7c
Ahmad <ahmadhusain5002@gmail.com>
2025-09-11 22:06:53 +0200

    Replace the function name and prevent memory leaks

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -2,14 +2,18 @@ const chai = require('chai');
 const sinon = require('sinon');
 const sendPaymentRequestToApi = require('./3-payment');
 const Utils = require('./utils');
+const {afterEach} = require('mocha');
 
 const expect = chai.expect;
+afterEach(() => {
+  sinon.restore();
+});
 
 describe('Testing how a function used', function () {
   it('tests how Utils.calculateNumber used', function () {
     const calculateNumberStats = sinon.spy(Utils, "calculateNumber");
     sendPaymentRequestToApi(100, 20);
-    calculateNumberStats.usedWith = calculateNumberStats.calledWith;
+    calculateNumberStats.usedWith = calculateNumberStats.calledWith.bind(calculateNumberStats);
     expect(calculateNumberStats.usedWith('SUM', 100, 20)).to.be.true;
   });
 });
