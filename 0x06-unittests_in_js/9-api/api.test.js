d6c34220dbb63ff81eed35cecb7aa646b373018f
Ahmad <ahmadhusain5002@gmail.com>
2025-09-11 23:35:45 +0200

    Clean up the mess

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,53 +1,43 @@
 const requests = require('request');
-const sinon = require('sinon');
 const chai = require('chai');
 
 const expect = chai.expect;
 
 describe('Test API', function () {
-  it('tests status code', function () {
+  it('tests index page status code', function () {
     requests.get('http://localhost:7865/', (error, response, body) => {
       if (error) {
-        throw new Error();
-      } else if (response) {
-        expect(response.statusCode).to.equal(200);
+        throw error;
       }
-    });
-  });
-  it('tests body content', function () {
-    requests.get('http://localhost:7865/', (error, response, body) => {
-      if (error) {
-        throw new Error();
-      } else if (body) {
-        expect(body).to.equal('Welcome to the payment system');
-      } else {
-        throw new Error();
-      }
+      expect(response.statusCode).to.equal(200);
     });
   });
 
-  it('tests the presence of GET /cart/:id', function () {
+  it('tests index page body content', function () {
+    requests.get('http://localhost:7865/', (error, response, body) => {
+      if (error) {
+        throw error;
+      }
+      expect(body).to.equal('Welcome to the payment system');
+    });
+  });
+
+  it('GET /cart/:id works with numbers', function () {
     requests.get('http://localhost:7865/cart/100', (error, response, body) => {
       if (error) {
-        throw new Error();
-      } else if (response) {
-        expect(response.statusCode).to.equal(200);
+        throw error;
       }
-      if (body && response.statusCode === 200) {
-        expect(body).to.equal('Payment methods for cart 100');
-      } else if (body && response.statusCode === 404) {
-        expect(body).to.not.equal('Payment methods for cart 100');
-      }
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Payment methods for cart 100');
     });
   });
 
-  it('tests response of GET /cart/:id', function () {
+  it('GET /cart/:id doesn\'t work with arguments other than numbers', function () {
     requests.get('http://localhost:7865/cart/invalidID', (error, response, body) => {
       if (error) {
-        throw new Error();
-      } else if (response) {
-        expect(response.statusCode).to.equal(404);
+        throw error;
       }
+      expect(response.statusCode).to.equal(404);
     });
   });
 });
