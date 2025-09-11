65590143d528642bcaf2e55c7186996290730cd4
Ahmad <ahmadhusain5002@gmail.com>
2025-09-12 00:05:48 +0200

    Clean up the mess

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -8,54 +8,44 @@ describe('Test API', function () {
   it('tests status code', function () {
     requests.get('http://localhost:7865/', (error, response, body) => {
       if (error) {
-        throw new Error();
-      } else if (response) {
-        expect(response.statusCode).to.equal(200);
+        throw error;
       }
+      expect(response.statusCode).to.equal(200);
     });
   });
   it('tests body content', function () {
     requests.get('http://localhost:7865/', (error, response, body) => {
       if (error) {
-        throw new Error();
-      } else if (body) {
-	expect(response.statusCode).to.equal(200);
-        expect(body).to.equal('Welcome to the payment system');
-      } else {
-        throw new Error();
+        throw error;
       }
+	  expect(response.statusCode).to.equal(200);
     });
   });
 
   it('tests response of GET /cart/:number', function () {
     requests.get('http://localhost:7865/cart/100', (error, response, body) => {
       if (error) {
-        throw new Error();
-      } else if (response) {
-        expect(response.statusCode).to.equal(200);
+        throw error;
       }
-      if (body) {
-        expect(body).to.equal('Payment methods for cart 100');
-      } else {
-        throw new Error();
-      }
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Payment methods for cart 100');
     });
   });
 
   it('tests response of GET /cart/:notANumber', function () {
     requests.get('http://localhost:7865/cart/invalid', (error, response, body) => {
       if (error) {
-        throw new Error();
-      } else if (response) {
-        expect(response.statusCode).to.equal(404);
+        throw error;
       }
+      expect(response.statusCode).to.equal(404);
     });
   });
 
   it('tests /available_payments', function () {
     requests.get('http://localhost:7865/available_payments', (error, response, body) => {
+      if (error) throw error;
       expect(response.statusCode).to.equal(200);
-      expect(body).to.equal('{"payment_methods":{"credit_cards":true,"paypal":false}}');
+      expect(JSON.parse(body)).to.deep.equal({"payment_methods":{"credit_cards":true,"paypal":false}});
     });
   });
 
@@ -66,7 +56,7 @@ describe('Test API', function () {
      };
      requests.post(options, (error, response, body) => {
        if (error) {
-         throw new Error();
+         throw error;
        }
        expect(response.statusCode).to.equal(200);
        expect(body).to.equal('Welcome Ahmad');
