40c3284f8b8754dc72e85ae4d4628f981031dafc
Ahmad <ahmadhusain5002@gmail.com>
2025-09-11 23:09:20 +0200

    separate tests

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,17 +1,26 @@
 const requests = require('request');
 const sinon = require('sinon');
 const chai = require('chai');
-
 const expect = chai.expect;
 
+
 describe('Test API', function () {
-  it('tests root route', function () {
+  it('tests root route status code', function () {
     requests.get('http://localhost:7865/', (error, response, body) => {
       if (error) {
-        throw new Error();
+        throw error;
       } else if (response) {
         expect(response.statusCode).to.equal(200);
-        expect(body).to.contain('Welcome to the payment system');
+      }
+    });
+  });
+
+  it('tests root route response body', function() {
+    requests.get("http://localhost:7865/", (error, res, body) => {
+      if (error) {
+        throw error;
+      } else {
+	expect(body).to.equal('Welcome to the payment system');
       }
     });
   });
