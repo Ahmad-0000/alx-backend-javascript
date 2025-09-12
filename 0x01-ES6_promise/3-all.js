778668af15334afea61d45fa015930a307751d49
Ahmad <ahmadhusain5002@gmail.com>
2025-09-12 10:00:51 +0200

    Remove a suspected function name

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,8 +1,8 @@
-import { uploadPhoto, createUserAccount } from './utils';
+import { uploadPhoto, createAnAccount } from './utils';
 
 export default function handleProfileSignup() {
   const photo = uploadPhoto();
-  const account = createUserAccount();
+  const account = createAnAccount();
 
   return Promise.all([photo, account]).then((array) => {
     console.log(`${array[0].body} ${array[1].firstName} ${array[1].lastName}`);
