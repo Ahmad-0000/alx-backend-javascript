67849140f5eadbd6d097d64ea98aedc511f48f8b
Ahmad <ahmadhusain5002@gmail.com>
2025-09-12 09:50:17 +0200

    utilize a custom iterable

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,3 +1,10 @@
 export default function iterateThroughObject(reportWithIterator) {
-  return reportWithIterator.join(' | ');
+  let string = "";
+  const iterator = reportWithIterator[Symbol.iterator]();
+  let {value} = iterator.next();
+  string += value;
+  while ({value} = iterator.next()) {
+    string += ` | ${value}`;
+  }
+  return string;
 }
