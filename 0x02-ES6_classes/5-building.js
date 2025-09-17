93142d01e6b82eb809e3bc2f39ba7dd573c4a8ec
Ahmad <ahmadhusain5002@gmail.com>
2025-09-17 18:45:35 +0200

    Make "Building" class an abstract class

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,6 +1,6 @@
 export default class Building {
   constructor(sqft) {
-    if (typeof this.evacuationWarningMessage !== 'function' && new.target !== Building) {
+    if (typeof this.evacuationWarningMessage !== 'function' || new.target === Building) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
